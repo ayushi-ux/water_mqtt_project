d5197fa3e2dfc16e97a155fa0f02e62d35c3fad9
ayushi-ux <jilupatel2004@gmail.com>
2026-02-15 13:14:46 +0530

    fix allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "local-dev-secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
